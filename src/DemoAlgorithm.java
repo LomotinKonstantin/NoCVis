addcbdafb131c2bfdc38c6874f957023b11a9759
Konstantin Lomotin <ke.lomotin@gmail.com>
2018-04-05 23:04:28 +0300

    Реализован тестовый алгоритм поиска в ширину
    Signed-off-by: Konstantin Lomotin <ke.lomotin@gmail.com>

--- src/DemoAlgorithm.java
+++ src/DemoAlgorithm.java
@@ -8,12 +8,13 @@ public class DemoAlgorithm implements Algorithm {
  *  Алгоритм поиска, основанный на обходе дерева в ширину.
  *  Посещенные вершины отбрасываются.
  *  **/
-    protected int nodes_num;
-    protected int start, stop;
-    protected int[] components;
-    protected int[] path;
+    protected int nodes_num;                        // число узлов
+    protected int start, stop;                      // номера стартового и конечного узлов
+    protected int[] components;                     // образующие
+    protected int[] path;                           // найденный путь
     protected boolean finished;
-    ArrayList<Integer> visited;
+    private ArrayList<ArrayList<Integer>> tree;     // список всех рассчитанных путей
+    private int curr_branch;                        // текущий маршрут для визуализации
 
     DemoAlgorithm(int nodes_num, int[] components, int start, int stop) {
         this.nodes_num = nodes_num;
@@ -22,8 +23,18 @@ public class DemoAlgorithm implements Algorithm {
         this.stop = min(nodes_num - 1, stop);
         finished = false;
 
-        visited = new ArrayList<>();
-        visited.add(start);
+        tree = new ArrayList<>();
+
+        buildTree();
+
+        curr_branch = 0;
+    }
+
+
+    /* Удобное преобразование вектора Integer в массив int */
+    public static int[] arrlToArr(ArrayList<Integer> arrl) {
+        Integer[] temp = arrl.toArray(new Integer[0]);
+        return Arrays.stream(temp).mapToInt(Integer::intValue).toArray();
     }
 
     @Override
@@ -34,19 +45,59 @@ public class DemoAlgorithm implements Algorithm {
     @Override
     public int[] nextStep() {
         if (!finished) {
-            int next = visited.get(visited.size() - 1) + components[0];
-            visited.add(next);
-            if (next == stop) {
+            if (curr_branch < tree.size() - 1)
+                return arrlToArr(tree.get(curr_branch++));
+            else {
                 finished = true;
+                return path;
             }
-            Integer[] temp = visited.toArray(new Integer[0]);
-            path = Arrays.stream(temp).mapToInt(Integer::intValue).toArray();
-        }
-        return path;
+        } else
+            return path;
     }
 
     @Override
     public boolean isFinished() {
         return finished;
+    }
+
+    /* Поиск пути в ширину. Все промежуточные шаги сохраняюся в tree */
+    private void buildTree() {
+        ArrayList<Integer> visited = new ArrayList<>();
+        visited.add(start);
+        {
+            ArrayList<Integer> branch = new ArrayList<>();
+            branch.add(start);
+            tree.add(branch);
+        }
+        int i = 0;
+        while (i < tree.size()){
+            ArrayList<Integer> branch = tree.get(i);
+            ++i;
+            int last = branch.get(branch.size() - 1);
+            for (int c : components) {
+                int next_a = last + c;
+                next_a -= (next_a >= nodes_num) ? nodes_num : 0;
+                if (!visited.contains(next_a)) {
+                    ArrayList<Integer> new_branch = new ArrayList<>(branch);
+                    new_branch.add(next_a);
+                    tree.add(new_branch);
+                    if (next_a == stop) {
+                        path = arrlToArr(new_branch);
+                        return;
+                    }
+                }
+                int next_b = last - c;
+                next_b += (next_b < 0) ? nodes_num : 0;
+                if (!visited.contains(next_b)) {
+                    ArrayList<Integer> new_branch = new ArrayList<>(branch);
+                    new_branch.add(next_b);
+                    tree.add(new_branch);
+                    if (next_b == stop) {
+                        path = arrlToArr(new_branch);
+                        return;
+                    }
+                }
+            }
+        }
     }
 }

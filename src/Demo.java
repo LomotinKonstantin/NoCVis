4ad7da9f8f0080031f58003f8077af250b0ae57b
Konstantin Lomotin <ke.lomotin@gmail.com>
2018-04-05 23:57:49 +0300

    Оформлен главный файл демонстрации
    Signed-off-by: Konstantin Lomotin <ke.lomotin@gmail.com>

--- src/Demo.java
+++ src/Demo.java
@@ -6,14 +6,25 @@ import java.awt.*;
  */
 public class Demo extends JFrame{
 
+    private static CircGraphPane cgp;
+
+    /* Параметры демонстрации */
+////////////////////////////////////////////////////
+    /* Параметры графа */
+    private static int NUMBER_OF_NODES = 41;        // Число вершин в графе
+    private static int[] COMPONENTS = {3, 17};   // Образующие
+
+    /* Параметры запуска */
+    private static int START = 0;                   // Номер стартовой вершины
+    private static int DEST = 11;                   // Номер конечной вершины
+    private static int INTERVAL_MS = 400;           // Количество миллисекунд на шаг
+////////////////////////////////////////////////////
+
     Demo() {
         super();
-        int[] comp = {1, 37};
-        int num = 80;
         setLayout(new BoxLayout(getContentPane(), BoxLayout.LINE_AXIS));
-        CircGraphPane cgp = new CircGraphPane(num, comp);
-        cgp.setAlgorithm(new DemoAlgorithm(num, comp, 0, 59));
-        cgp.start(50);
+        cgp = new CircGraphPane(NUMBER_OF_NODES, COMPONENTS);
+        cgp.setAlgorithm(new DemoAlgorithm(NUMBER_OF_NODES, COMPONENTS, START, DEST));
         add(cgp);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(new Dimension(600, 600));
@@ -22,5 +33,12 @@ public class Demo extends JFrame{
     public static void main(String[] argv) {
         Demo d = new Demo();
         d.setVisible(true);
+
+        /* Запуск визуализации */
+        /* Раскомментировать одну строку из двух! */
+/////////////////////////////////////////////////////////
+        cgp.start(INTERVAL_MS); // на каждый шаг отводится INTERVAL_MS миллисекунд
+//        cgp.start();            // шаги переключаются по щелчку
+/////////////////////////////////////////////////////////
     }
 }
